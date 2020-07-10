3fb75ae64bb9716847455d460e20475bc465c2fc
Andre Serudo <andreserudo@gmail.com>
2020-07-10 17:38:21 -0400

    adding responsive and click event

--- src/main.js
+++ src/main.js
@@ -1,3 +1,12 @@
 function seeShareMedia(){
-  console.log('oin');
+  var element = document.getElementById('contentShare');
+
+  
+  if(element.style.getPropertyValue("opacity") === "1"){
+    element.style.setProperty("visibility","hidden");
+    element.style.setProperty("opacity", "0");  
+  }else{
+    element.style.setProperty("visibility","visible");
+    element.style.setProperty("opacity", "1");  
+  }
 }
